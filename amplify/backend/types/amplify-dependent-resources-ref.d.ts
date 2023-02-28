17fa7acc16848ecee6cce7e9325c27eda13a2a54
Your Name <you@example.com>
2023-02-28 22:51:13 +0530

    Pushed to the amplify\

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,20 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "analytics": {
+    "mypizzastorewithpinp": {
+      "Id": "string",
+      "Region": "string",
+      "appName": "string"
+    }
+  },
+  "auth": {
+    "mypizzastorewithpinp7beafa8b": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
